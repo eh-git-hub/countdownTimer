e143e89a5c5d543f303e0f5346956facf600970d
Edgar Hernandez <EdgarHomeFolder@Edgars-MacBook-Pro.local>
2021-04-30 11:48:34 -0500

    reformatted js to meet application needs

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
-// Set the date we're counting down to
-var countDownDate = new Date("May 1, 2021 15:37:25").getTime();
+function countdown(minutes) {
+  let seconds = 60;
+  let mins = minutes
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = "-" + days + "d " + hours + ":"
-  + minutes + ":" + seconds + "s ";
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+  function tick() {
+      //This script expects an element with an ID = "counter". You can change that to what ever you want. 
+      let counter = document.getElementById("counter");
+      let currentMinutes = mins-1
+      seconds--;
+      //Ternary Operator(?): Assigns a value to a variable based on a condition.
+      counter.innerHTML = currentMinutes.toString() + ":" + (seconds < 10 ? "0" : "") + seconds.toString() + "s";
+      if(seconds > 0) {
+          setTimeout(tick, 1000);
+      } else if (mins > 1) {
+              countdown(mins-1);
+      } else if (seconds <= 0) {
+        counter.innerHTML = "TIME'S UP!"
+      }
   }
-}, 1000);
+  tick();
+}
+
+countdown(2);
